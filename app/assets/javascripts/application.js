99f7388a406d613cd2cd843506e7596e2479a55b
Savelya <savabezenkov@mail.ru>
2019-11-16 03:28:12 +0300

    Third task

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,8 @@
 //= require jquery_ujs
 //= require_tree .
 //= require_self
-
+//= require 'icheck'
+//= require bootstrap-sprockets
    
 
 $(document).ready(function() {
@@ -28,7 +29,34 @@ $(document).ready(function() {
         $("#form").show();
         $("#show").hide();
     });
+    
+    $(".icheck-me").on("ifChecked", function(event){
+        $(this).parent().parent().children(".todo-text").css("text-decoration", "line-through");
+    });
+
+    $(".icheck-me").on("ifUnchecked", function(event){
+        $(this).parent().parent().children(".todo-text").css("text-decoration", "none");
+    });
+
 });
+
+function icheck(){
+    if($(".icheck-me").length > 0){
+      $(".icheck-me").each(function(){
+        var $el = $(this);
+        var skin = ($el.attr('data-skin') !== undefined) ? "_" + $el.attr('data-skin') : "",
+        color = ($el.attr('data-color') !== undefined) ? "-" + $el.attr('data-color') : "";
+        var opt = {
+          checkboxClass: 'icheckbox' + skin + color,
+        }
+        $el.iCheck(opt);
+      });
+    }
+}
+  
+$(function(){
+    icheck();
+})
 
 
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
     def index
-        @projects = Project.all
+        @projects = Project.includes(:todos)
     end
 
     def create
