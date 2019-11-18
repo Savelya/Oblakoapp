02afae900b39a2b03829c2d1b53a30f1a9fba586
Savelya <savabezenkov@mail.ru>
2019-11-19 01:39:17 +0300

    Third task done

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Oblakoapp
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     config.serve_static_assets = true
+    config.assets.compile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
