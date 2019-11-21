46b7cb85618900b6d9b308ca3558d3105798d0fc
Savelya <savabezenkov@mail.ru>
2019-11-22 02:31:35 +0300

    Add json response

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
     def index
         @projects = Project.includes(:todos)
+        respond_to do |format|
+            format.html # show.html.erb
+            format.json { render json: @projects }
+           end
     end
 end
