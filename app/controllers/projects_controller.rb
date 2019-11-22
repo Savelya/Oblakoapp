373d47f08ef8c6ee193948e097ba5cb65577baa6
Savelya <savabezenkov@mail.ru>
2019-11-22 04:26:57 +0300

    Todo json

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,11 @@
 class ProjectsController < ApplicationController
     def index
-        @projects = Project.all
+        @projects = Project.includes(:todo)
         respond_to do |format|
             format.html # show.html.erb
-            format.json { render json: @projects }
+            format.json { 
+                render json: @projects
+             }
            end
     end
 end
